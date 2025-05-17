975c9d1dfc49273c6fd374d4d861086c2478a74d
nurbo323 <242214@astanait.edu.kz>
2025-05-17 10:56:05 +0500

    Add Edge class with source, destination and weight

--- src/main/java/graph/Edge.java
+++ src/main/java/graph/Edge.java
@@ -1,4 +1,42 @@
 package graph;
 
-public class Edge {
+public class Edge<T> {
+    private Vertex<T> source;
+    private Vertex<T> destination;
+    private double weight;
+
+    public Edge(Vertex<T> source, Vertex<T> destination) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = 0;
+    }
+
+    public Edge(Vertex<T> source, Vertex<T> destination, double weight) {
+        this(source, destination);
+        this.weight = weight;
+    }
+
+    public Vertex<T> getSource() {
+        return source;
+    }
+
+    public void setSource(Vertex<T> source) {
+        this.source = source;
+    }
+
+    public Vertex<T> getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Vertex<T> destination) {
+        this.destination = destination;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
 }
