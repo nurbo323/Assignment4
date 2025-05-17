01695a21216da9f51ad327044a6d53a32a97394a
nurbo323 <242214@astanait.edu.kz>
2025-05-17 11:21:47 +0500

    Update BFS.java

--- src/main/java/graph/BFS.java
+++ src/main/java/graph/BFS.java
@@ -1,4 +1,25 @@
 package graph;
 
-public class BFS {
+import java.util.*;
+
+public class BFS<T> {
+    public void traverse(Vertex<T> start) {
+        Set<Vertex<T>> visited = new HashSet<>();
+        Queue<Vertex<T>> queue = new LinkedList<>();
+
+        visited.add(start);
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            Vertex<T> current = queue.poll();
+            System.out.println("Visited: " + current.getData());
+
+            for (Vertex<T> neighbor : current.getAdjacentVertices().keySet()) {
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
 }
